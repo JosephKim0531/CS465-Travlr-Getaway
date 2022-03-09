1ff33cef225c90a02f9f01500af9e9ed3f3ec153
unknown <josepjk1@gmail.com>
2022-03-08 21:29:18 -0800

    Use MVC pattern to manage application logic

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var aboutRouter = require('./app_server/routes/about');
+var contactRouter = require('./app_server/routes/contact');
+var mealsRouter = require('./app_server/routes/meals');
 var app = express();
 
 // view engine setup
@@ -29,6 +31,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/meals', mealsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
