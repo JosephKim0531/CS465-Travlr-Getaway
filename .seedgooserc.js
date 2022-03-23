16687a43184456c50bf8cd99658c438ba69bf414
unknown <josepjk1@gmail.com>
2022-03-22 17:50:32 -0700

    NoSQL Databases, Models, and Schemas

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,18 @@
-module.exports = {
+/*module.exports = {
     modelBaseDirectory: 'app_server/database/models', //model directory name
     models: ['*.js', '!db.js'], //model matcher
     data:'data', //data directory name
     db:'mongodb://localhost:27017/travlr' //db connection url
-}
+}; */
+
+module.exports = {
+
+  modelBaseDirectory: "app_server/database/models", // model directory name
+
+  models: ["*.js", "!db.js"], // model matcher
+
+  data: "data", // data directory name
+
+  db: "mongodb://localhost:27017/travlr", // db connection url
+
+};
